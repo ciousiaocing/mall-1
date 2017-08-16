405803787b888943601595908bbdad49691c6f4b
ciou <yyouko22@gmail.com>
2017-08-16 19:58:33 +0800

    可以新增商品到購物車

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 RSpec.describe Cart, type: :model do
 
   describe "購物車基本功能" do
-    #* 可以新增商品到購物車裡，然後購物車裡就有東西了
+    it "可以新增商品到購物車裡，然後購物車裡就有東西了" do
+
+      cart = Cart.new
+      cart.add_item(1)
+      expect(cart.empty?).to be false
+
+    end
     #* 如果加了相同種類的商品，購買項目(CartItem)並不會增加，但數量會改變。
     #* 商品可以放到購物車裡，也可以再拿出來
     #* 可以計算整個購物車的總消費金額
@@ -15,5 +21,5 @@ RSpec.describe Cart, type: :model do
       #* 也可以把 Hash 還原成購物車的內容
   end
 
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
